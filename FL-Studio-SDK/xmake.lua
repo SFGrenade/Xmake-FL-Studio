9a5102dd453e45d9b7e29b7458060209426e8eec
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2026-07-11 22:48:55 +0200

    FL-Studio-SDK: add version 20210311b

---
+++ FL-Studio-SDK/xmake.lua
@@ -0,0 +1,14 @@
+
+target( "FL-Studio-SDK" )
+    set_kind( "static" )
+
+    set_version( "2021.03.11", { build = "20210311b", soname = true } )
+
+    set_default( false )
+    set_group( "LIBS" )
+
+    add_includedirs( "include", { public = true } )
+
+    add_headerfiles( "include/(*.h)" )
+
+    add_files( "src/*.cpp" )
